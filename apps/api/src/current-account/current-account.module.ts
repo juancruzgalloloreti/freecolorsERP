e5ee3001a8bf02fd6bb91cc41f2ff7d3b1472110
juancruzgalloloreti <juancruzgalloloreti@gmail.com>
2026-05-15 13:21:35 -0300

    update PermissionsModule

--- apps/api/src/current-account/current-account.module.ts
+++ apps/api/src/current-account/current-account.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PermissionsModule } from '../permissions/permissions.module';
 import { CurrentAccountController } from './current-account.controller';
 import { CurrentAccountService } from './current-account.service';
 
 @Module({
+  imports: [PermissionsModule],
   controllers: [CurrentAccountController],
   providers: [CurrentAccountService],
   exports: [CurrentAccountService],
--- apps/api/src/price-lists/price-lists.module.ts
+++ apps/api/src/price-lists/price-lists.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PermissionsModule } from '../permissions/permissions.module';
 import { PriceListsController } from './price-lists.controller';
 import { PriceListsService } from './price-lists.service';
 
 @Module({
+  imports: [PermissionsModule],
   controllers: [PriceListsController],
   providers: [PriceListsService],
   exports: [PriceListsService],
